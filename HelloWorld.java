045067932c610189143b2e4320b6444d7eac64bb
johnbunni <150174311+johnbunni@users.noreply.github.com>
2023-11-07 14:19:49 -0500

    Hello world john
    I added on to the Hello World and added a sentence with my name in it.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    
+    System.out.println("Hi, my name is John and I am amazing!");
   }
 }
